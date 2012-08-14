3195910fa9ec813e86310372ec5a4d764e61bd65
Edmilson <edmilsonlani@gmail.com>
2012-08-14 20:58:32 -0300

    Adicionando Quartos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colchonet::Application.config.secret_token = '5507260d1e307dab0461ed6df5a0cba7dbd9ca1e93afc78b1c39cd443c58daf3f03a24e5d7f8a2c8c78e69290b983cbb3738fefcd0f634ce5bdaf7d1b4d2fd71'
